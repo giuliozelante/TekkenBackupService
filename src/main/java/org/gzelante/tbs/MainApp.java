0efc31b7cd81cf4aa1ef2203aa2d4b20715c430d
Giulio Zelante <giulio.zelante@alkemy.com>
2020-12-19 09:51:02 +0100

    WIP

--- src/main/java/org/gzelante/tbs/MainApp.java
+++ src/main/java/org/gzelante/tbs/MainApp.java
@@ -16,7 +16,7 @@ public class MainApp extends Application {
         Scene scene = new Scene(root);
 //        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
 
-        stage.setTitle("Tekken 7 Backup Service");
+        stage.setTitle("Tekken 7 Backup Service - Alpha");
         stage.setScene(scene);
         stage.show();
     }
